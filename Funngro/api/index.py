2a7ab79858438b6dbb6762fcd10f82fa4089e028
Yashwant990 <dpnayak333@gmail.com>
2026-01-21 14:26:09 +0530

    Refactor import statements in index.py

--- Funngro/api/index.py
+++ Funngro/api/index.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# ✅ Add root project folder path (so it can find app.py)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app import app as application
+from app import app
